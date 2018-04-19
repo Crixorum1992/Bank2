1ada542eccf77b148fbb937b6b71f4c61c612584
Nicholas Reid <nicjreid@ut.utm.edu>
2018-04-19 15:31:30 -0500

    update

--- Bank2/Class1.cs
+++ Bank2/Class1.cs
@@ -54,7 +54,7 @@ namespace BankAccountNS
                 throw new ArgumentOutOfRangeException("amount");
             }
 
-            m_balance = amount;
+            m_balance += amount;
         }
 
         public void Credit(double amount)
